d073626bbbb183f9f2eb8dcc10767f1ccf4d7e00
pythops <contact@pythops.com>
2022-04-02 22:01:26 +0200

    Add slack bot conf

--- app/config.py
+++ app/config.py
@@ -3,9 +3,13 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "34xULHbQt4Ns6P29KsWhX4SRm7rypJyxhFL")
+
     OPENAI_URL = "https://api.openai.com/v1/answers"
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     OPENAI_MODEL = os.getenv("OPENAI_MODEL", "curie")
+
+    SLACK_APP_TOKEN = os.getenv("SLACK_APP_TOKEN")
+    SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
 
 
 config = Config()
