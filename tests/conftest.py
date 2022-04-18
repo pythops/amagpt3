d8d0248970d52781d186cf8cc8ca108aa9104ea9
pythops <contact@pythops.com>
2022-04-18 14:36:36 +0200

    use pytest_asyncio.fixture instead of pytest.fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,19 @@
 import pytest
+import pytest_asyncio
 
 from app.api import init_api
 from app.website import init_website
 
 
 @pytest.mark.asyncio
-@pytest.fixture
+@pytest_asyncio.fixture
 async def api():
     api = init_api()
     yield api.test_client()
 
 
 @pytest.mark.asyncio
-@pytest.fixture
+@pytest_asyncio.fixture
 async def website():
     website = init_website()
     yield website.test_client()
